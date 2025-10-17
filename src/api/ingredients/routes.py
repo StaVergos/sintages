9ed2d7405a3eea42ec7cdebafb35df22ed3604d5
G.Perdikaris <perdgeorge@gmail.com>
2025-10-18 00:20:27 +0300

    Removed error handler from ingredients route

--- src/api/ingredients/routes.py
+++ src/api/ingredients/routes.py
@@ -23,8 +23,6 @@ async def get_ingredient(
     ingredient_repository: IngredientRepository = Depends(get_ingredient_repository),
 ):
     ingredient = ingredient_repository.get_ingredient_by_id(ingredient_id)
-    if ingredient is None:
-        raise HTTPException(status_code=404, detail="Ingredient not found")
     return ingredient
 
 
