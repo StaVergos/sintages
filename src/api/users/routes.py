77ad2714ac684b791e3e65498d7ac547ab5489eb
G.Perdikaris <perdgeorge@gmail.com>
2025-10-18 22:50:14 +0300

    Provide error responses dict for Users

--- src/api/users/routes.py
+++ src/api/users/routes.py
@@ -2,8 +2,16 @@ from fastapi import APIRouter, Depends
 from src.api.users.schemas import CreateUserSchema, GetUserSchema, UpdateUserSchema
 from src.api.users.services import UserRepository
 from src.api.users.dependencies import get_user_repository
+from src.core.schemas import ErrorResponse
 
 router = APIRouter()
+
+error_responses = {
+    404: {"model": ErrorResponse, "description": "User not found"},
+    409: {"model": ErrorResponse, "description": "User already exists"},
+    422: {"model": ErrorResponse, "description": "Invalid user input format"},
+    500: {"model": ErrorResponse, "description": "Internal server error"},
+}
 
 
 @router.get("/", response_model=list[GetUserSchema])
@@ -21,7 +29,12 @@ async def get_user(
     return user
 
 
-@router.post("/", response_model=GetUserSchema, status_code=201)
+@router.post(
+    "/",
+    response_model=GetUserSchema,
+    status_code=201,
+    responses=error_responses,
+)
 async def create_user(
     user: CreateUserSchema,
     user_repository: UserRepository = Depends(get_user_repository),
@@ -29,7 +42,11 @@ async def create_user(
     return user_repository.create_user(user)
 
 
-@router.put("/{user_id}", response_model=GetUserSchema)
+@router.put(
+    "/{user_id}",
+    response_model=GetUserSchema,
+    responses=error_responses,
+)
 async def update_user(
     user_id: int,
     user: UpdateUserSchema,
