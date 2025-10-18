b2ea00e539f2b230318e53284a32a4f21a422c38
Gap <perdgeorge@gmail.com>
2025-10-18 19:35:26 +0300

    Remove unused import in categories

--- src/api/categories/routes.py
+++ src/api/categories/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from src.api.categories.schemas import (
     CreateCategorySchema,
     GetCategorySchema,
