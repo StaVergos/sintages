162d02a0cd5eef78369e01af57f44a81c7d5a64a
G.Perdikaris <perdgeorge@gmail.com>
2025-10-23 01:17:44 +0300

    Fixing nullables and adding quantity proxy

--- src/db/models/recipes.py
+++ src/db/models/recipes.py
@@ -1,7 +1,8 @@
-from typing import Optional, List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING
 from src.db.base import Base, TimestampMixin
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.ext.associationproxy import association_proxy
 from src.db.models.users import User
 
 if TYPE_CHECKING:
@@ -22,11 +23,15 @@ class Recipe(Base, TimestampMixin):
     portions: Mapped[int] = mapped_column(nullable=False)
     is_vegan: Mapped[bool] = mapped_column(nullable=False, default=False)
     instructions: Mapped[str] = mapped_column(nullable=False)
-    ingredients: Mapped[List["Ingredient"]] = relationship(back_populates="recipe")
-    user_id: Mapped[Optional[int]] = mapped_column(
-        ForeignKey("users.id"), nullable=True
+    recipe_ingredients = relationship("RecipeIngredient", back_populates="recipe")
+    ingredients: Mapped[List["Ingredient"]] = relationship(
+        "Ingredient", secondary="recipe_ingredients", back_populates="recipe"
     )
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     user: Mapped["User"] = relationship(back_populates="recipes")
+    quantity = association_proxy(
+        target_collection="recipe_ingredients", attr="quantity"
+    )
 
     @property
     def name(self) -> str:
@@ -35,3 +40,14 @@ class Recipe(Base, TimestampMixin):
     @name.setter
     def name(self, value: str) -> None:
         self._name = value.lower()
+
+
+class RecipeIngredient(Base):
+    __tablename__ = "recipe_ingredients"
+    recipe_id: Mapped[int] = mapped_column(ForeignKey("recipes.id"), primary_key=True)
+    ingredient_id: Mapped[int] = mapped_column(
+        ForeignKey("ingredients.id"), primary_key=True
+    )
+    quantity: Mapped[str] = mapped_column(nullable=False)
+    recipe: Mapped["Recipe"] = relationship(back_populates="recipe_ingredients")
+    ingredient: Mapped["Ingredient"] = relationship(back_populates="recipe_ingredients")
