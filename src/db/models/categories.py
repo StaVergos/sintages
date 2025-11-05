f36ccaa9e9356e583c45c54c4b519b77f385892e
G.Perdikaris <perdgeorge@gmail.com>
2025-11-06 00:47:21 +0200

    Fixing categories models

--- src/db/models/categories.py
+++ src/db/models/categories.py
@@ -27,7 +27,3 @@ class Category(Base, TimestampMixin):
     @name.setter
     def name(self, value: str) -> None:
         self._name = value.lower()
-
-    @property
-    def ingredient_ids(self) -> List[int]:
-        return [ingredient.id for ingredient in self.ingredients]
