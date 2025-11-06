713ee78ad333505b75d3da6dac9737dd57349ea5
G.Perdikaris <perdgeorge@gmail.com>
2025-11-06 22:48:06 +0200

    Creating common schema for categories ingredients. Commented out pre-existing schemas

--- src/api/ingredients/schemas.py
+++ src/api/ingredients/schemas.py
@@ -1,52 +1,52 @@
-from datetime import datetime
-from pydantic import Field, field_validator, field_serializer
-from src.api.schemas import BaseSchema
-from src.api.categories.schemas import CategoryRelationshipSchema
+# from datetime import datetime
+# from pydantic import Field, field_validator, field_serializer
+# from src.api.schemas import BaseSchema
+# from src.api.categories.schemas import CategoryRelationshipSchema
 
 
-class IngredientSchema(BaseSchema):
-    name: str = Field(max_length=50, examples=["Broccoli"])
-    is_vegan: bool = Field(..., examples=[True])
-    categories: list["CategoryRelationshipSchema"] = Field(
-        default_factory=list,
-        examples=[[{"id": 1, "name": "Veggies"}]],
-    )
+# class IngredientSchema(BaseSchema):
+#     name: str = Field(max_length=50, examples=["Broccoli"])
+#     is_vegan: bool = Field(..., examples=[True])
+#     categories: list["CategoryRelationshipSchema"] = Field(
+#         default_factory=list,
+#         examples=[[{"id": 1, "name": "Veggies"}]],
+#     )
 
-    @field_validator("name")
-    @classmethod
-    def validate_name(cls, value: str) -> str:
-        return value.lower()
+#     @field_validator("name")
+#     @classmethod
+#     def validate_name(cls, value: str) -> str:
+#         return value.lower()
 
-    @field_serializer("name")
-    def serialize_name(self, value: str) -> str:
-        return value.capitalize()
+#     @field_serializer("name")
+#     def serialize_name(self, value: str) -> str:
+#         return value.capitalize()
 
-    # @property
-    # def categories(self) -> list["CategoryRelationshipSchema"]:
-    #     from src.api.categories.schemas import CategoryRelationshipSchema
+#     # @property
+#     # def categories(self) -> list["CategoryRelationshipSchema"]:
+#     #     from src.api.categories.schemas import CategoryRelationshipSchema
 
-    #     return list[CategoryRelationshipSchema]
-
-
-class GetIngredientSchema(IngredientSchema):
-    id: int = Field(..., examples=[1])
-    created_at: datetime = Field(..., examples=["2023-10-01T12:00:00Z"])
-    updated_at: datetime | None = Field(..., examples=["2023-10-01T12:00:00Z"])
+#     #     return list[CategoryRelationshipSchema]
 
 
-class CreateIngredientSchema(IngredientSchema):
-    pass
+# class GetIngredientSchema(IngredientSchema):
+#     id: int = Field(..., examples=[1])
+#     created_at: datetime = Field(..., examples=["2023-10-01T12:00:00Z"])
+#     updated_at: datetime | None = Field(..., examples=["2023-10-01T12:00:00Z"])
 
 
-class UpdateIngredientSchema(IngredientSchema):
-    name: str | None = Field(examples=["Broccoli"], default=None)
-    is_vegan: bool | None = Field(examples=[True], default=None)
-    categories: list["CategoryRelationshipSchema"] = Field(
-        default_factory=list,
-        examples=[[{"id": 1, "name": "Veggies"}]],
-    )
+# class CreateIngredientSchema(IngredientSchema):
+#     pass
 
 
-class IngredientRelationshipSchema(BaseSchema):
-    id: int = Field(..., examples=[1])
-    name: str = Field(max_length=50, examples=["Broccoli"])
+# class UpdateIngredientSchema(IngredientSchema):
+#     name: str | None = Field(examples=["Broccoli"], default=None)
+#     is_vegan: bool | None = Field(examples=[True], default=None)
+#     categories: list["CategoryRelationshipSchema"] = Field(
+#         default_factory=list,
+#         examples=[[{"id": 1, "name": "Veggies"}]],
+#     )
+
+
+# class IngredientRelationshipSchema(BaseSchema):
+#     id: int = Field(..., examples=[1])
+#     name: str = Field(max_length=50, examples=["Broccoli"])
