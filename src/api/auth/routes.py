43b304e9c8e03b148a14eeef2b8ec059bd803d91
G.Perdikaris <perdgeorge@gmail.com>
2025-11-15 19:50:08 +0200

    Fixing auth.routes

--- src/api/auth/routes.py
+++ src/api/auth/routes.py
@@ -1,11 +1,9 @@
-from datetime import datetime, timedelta
-from typing import Annotated
+from datetime import datetime, timedelta, timezone
 from fastapi import Depends, APIRouter, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
 
 from src.api.auth.services import AuthRepository
 from src.api.auth.dependencies import auth_repository
-from src.api.auth.schemas import Token
+from src.api.auth.schemas import Token, TokenData, LoginRequest
 from src.core.config import config
 
 router = APIRouter()
@@ -13,10 +11,12 @@ router = APIRouter()
 
 @router.post("/login", response_model=Token)
 async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    login_request: LoginRequest,
     auth_repository: AuthRepository = Depends(auth_repository),
 ) -> Token:
-    user = auth_repository.authenticate_user(form_data.username, form_data.password)
+    user = auth_repository.authenticate_user(
+        login_request.username, login_request.password
+    )
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -24,10 +24,11 @@ async def login_for_access_token(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expiration_timestamp = datetime.now(timezone.utc) + access_token_expires
 
-    access_token = auth_repository.create_access_token(
-        data={"sub": user.username},
-        expiration_timestamp=datetime.utcnow(),
-        expires_delta=access_token_expires,
+    data = TokenData(
+        username=user.username,
+        expiration_timestamp=expiration_timestamp.timestamp(),
     )
+    access_token = auth_repository.create_access_token(token_data=data)
     return Token(access_token=access_token, token_type="bearer")
