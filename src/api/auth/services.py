202ee71a7ba595c136dacf45df778398381e53ef
G.Perdikaris <perdgeorge@gmail.com>
2025-11-15 19:50:40 +0200

    Fix services for auth

--- src/api/auth/services.py
+++ src/api/auth/services.py
@@ -1,32 +1,19 @@
-from datetime import datetime, timedelta
 from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import jwt
 from jwt.exceptions import InvalidTokenError
-import os
 from pytest import Session
 from src.api.auth.schemas import TokenData
-from src.api.users.schemas import GetUserSchema, UserSchema
+from src.api.users.schemas import UserSchema
 from src.core.enums import ErrorKind
 from src.core.exceptions import ErrorException
 from src.core.security import verify_password
-from dotenv import load_dotenv, find_dotenv
 from src.db.models.users import User
 from src.core.config import config
 
-load_dotenv(find_dotenv())
-
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
-ALGORITHM = os.environ.get("ALGORITHM")
-
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-class UserInDB(UserSchema):
-    hashed_password: str
 
 
 class AuthRepository:
@@ -37,10 +24,10 @@ class AuthRepository:
     def repo_name(self) -> str:
         return "AuthRepository"
 
-    def get_user_by_username(self, username: str) -> GetUserSchema | None:
+    def get_user_by_username(self, username: str) -> User:
         user = self.db.query(User).filter(User.username == username).first()
         if user:
-            return GetUserSchema.model_validate(user)
+            return user
         else:
             raise ErrorException(
                 code=status.HTTP_404_NOT_FOUND,
@@ -62,20 +49,14 @@ class AuthRepository:
         return user
 
     def create_access_token(
-        data: TokenData,
-        expiration_timestamp: datetime,
-        expires_delta: timedelta | None = None,
+        self,
+        token_data: TokenData,
     ):
-        to_encode = data.model_copy()
-        if expires_delta:
-            expire = expiration_timestamp + expires_delta
-        else:
-            expire = expiration_timestamp + timedelta(
-                minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES
-            )
-        to_encode.update({"exp": int(expire.timestamp())})
+        payload_to_encode = token_data.model_copy()
         encoded_jwt = jwt.encode(
-            to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM
+            payload=payload_to_encode.model_dump(mode="json"),
+            key=config.SECRET_KEY,
+            algorithm=config.ALGORITHM,
         )
         return encoded_jwt
 
@@ -86,10 +67,11 @@ class AuthRepository:
         credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid username or password",
-            headers={"WWW-Authenticate": "Bearer"},
         )
         try:
-            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            payload = jwt.decode(
+                token, config.SECRET_KEY, algorithms=[config.ALGORITHM]
+            )
             username = payload.get("sub")
             if username is None:
                 raise credentials_exception
